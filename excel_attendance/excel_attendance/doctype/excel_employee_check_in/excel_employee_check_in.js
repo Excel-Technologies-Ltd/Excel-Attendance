96e248de1b91b71abaa2103a6597f7d9b5aca851
Shaid Azmin <excel.azmin@gmail.com>
2024-01-25 10:46:28 +0000

    working on device

--- excel_attendance/excel_attendance/doctype/excel_employee_check_in/excel_employee_check_in.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Shaid Azmin and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Excel Employee Check IN", {
-	refresh(frm) {
-
-	},
-});
